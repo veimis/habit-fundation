ee7b606070272b810c9536e84959830261530af0
moomoo14 <mouhamedbocar@yahoo.fr>
2019-06-23 12:32:25 +0800

    more progress

--- pages/progress/my_progress/progress.js
+++ pages/progress/my_progress/progress.js
@@ -3,6 +3,18 @@
 const app = getApp()
 
 Page({
+  DotStyle(e) {
+    this.setData({
+      DotStyle: e.detail.value
+    })
+  },
+  // cardSwiper
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+  
   ViewImage(e) {
     wx.previewImage({
       urls: this.data.imgList,
@@ -28,6 +40,35 @@ Page({
     });
   },
   data: {
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
     imgList: [],
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
