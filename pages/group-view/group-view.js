89e3a9acfcb5fdf8ad419303dd8916a5e95a4328
Ilkka Veima <ilkka.veima@gmail.com>
2019-06-23 10:21:51 +0800

    Add group view

---
+++ pages/group-view/group-view.js
@@ -0,0 +1,72 @@
+// group-view.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    avatarList:[
+      "/images/ninja.jpeg",
+      "/images/lady.jpeg",
+      "/images/man.jpeg",
+      "/images/manager.jpeg",
+      "/images/superman.jpeg"
+    ]
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
